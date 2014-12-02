7023602c9981eaf47940bb474a0605c99bca6138
Thomas Sjöholm <ThompZon@gmail.com>
2014-12-02 16:14:00 +0100

    fixed copy-paste empty wall follower

--- include/s8_go_straight_controller/go_straight_controller_node.h
+++ include/s8_go_straight_controller/go_straight_controller_node.h
@@ -1,26 +1,30 @@
-#ifndef __WALL_FOLLOWER_CONTROLLER_NODE_H
-#define __WALL_FOLLOWER_CONTROLLER_NODE_H
+#ifndef __GO_STRAIGHT_CONTROLLER_NODE_H
+#define __GO_STRAIGHT_CONTROLLER_NODE_H
 
 #include <string>
 #include <s8_motor_controller/motor_controller_node.h>
 #include <s8_ir_sensors/ir_sensors_node.h>
 
 namespace s8 {
-    namespace wall_follower_controller_node {
-        const std::string NODE_NAME =                       "s8_wall_follower_controller_node";
+    namespace go_straight_controller_node {
+        const std::string NODE_NAME =                       "s8_go_straight_controller_node";
 
         const std::string TOPIC_IR_DISTANCES =              s8::ir_sensors_node::TOPIC_IR_DISTANCES;
         const std::string TOPIC_TWIST =                     s8::motor_controller_node::TOPIC_TWIST;
         const std::string ACTION_STOP =                     s8::motor_controller_node::ACTION_STOP;
-        const std::string ACTION_FOLLOW_WALL =              "/s8/follow_wall";
+        const std::string ACTION_GO_STRAIGHT =              "/s8/go_straight";
 
-        enum FollowWallFinishedReason {
+        enum GoStraightFinishedReason {
             TIMEOUT,
-            OUT_OF_RANGE,
+            FOUND_WALL,
             PREEMPTED
         };
 
-        enum WallToFollow {
+        enum Direction {
+            EAST = s8::,
+            NORTH = 90,
+            WEST = 180,
+            SOUTH = 270
             LEFT = s8::motor_controller_node::RotationDirection::LEFT,
             RIGHT = s8::motor_controller_node::RotationDirection::RIGHT
         };
@@ -30,7 +34,6 @@ namespace s8 {
                 case WallToFollow::LEFT: return "LEFT";
                 case WallToFollow::RIGHT: return "RIGHT";
             }
-
             return "UNKNOWN";
         }
     }
--- src/go_straight_controller_node.cpp
+++ src/go_straight_controller_node.cpp
@@ -11,7 +11,6 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Pose2D.h>
 #include <s8_pose/pose_node.h>
-#include <s8_msgs/IRDistances.h>
 #include <s8_motor_controller/StopAction.h>
 #include <s8_go_straight_controller/GoStraightAction.h>
 
@@ -19,87 +18,33 @@
 
 #define TOPIC_POSE                  s8::pose_node::TOPIC_POSE_SIMPLE
 
-#define PARAM_FOLLOWING_KP_NAME         "following_kp"
-#define PARAM_FOLLOWING_KP_DEFAULT      10.0
-#define PARAM_FOLLOWING_KD_NAME         "following_kd"
-#define PARAM_FOLLOWING_KD_DEFAULT      4.0
-#define PARAM_FOLLOWING_KI_NAME         "following_ki"
-#define PARAM_FOLLOWING_KI_DEFAULT      0.0
-#define PARAM_DISTANCE_KI_NAME          "distance_ki"
-#define PARAM_DISTANCE_KI_DEFAULT       0.0
-#define PARAM_DISTANCE_KP_NAME          "distance_kp"
-#define PARAM_DISTANCE_KP_DEFAULT       10.0
-#define PARAM_DISTANCE_KD_NAME          "distance_kd"
-#define PARAM_DISTANCE_KD_DEFAULT       4.0
-#define PARAM_I_LIMIT_NAME              "i_limit"
-#define PARAM_I_LIMIT_DEFAULT           0.5
-#define PARAM_DISTANCE_NAME             "distance"
-#define PARAM_DISTANCE_DEFAULT          0.09
-#define PARAM_LINEAR_SPEED_NAME         "linear_speed"
-#define PARAM_LINEAR_SPEED_DEFAULT      0.2
-#define PARAM_IR_THRESHOLD_NAME         "ir_threshold"
-#define PARAM_IR_THRESHOLD_DEFAULT      0.2
-#define PARAM_ALIGNMENT_KP_NAME         "alignment_kp"
-#define PARAM_ALIGNMENT_KP_DEFAULT      0.1
-#define PARAM_ALIGNMENT_KD_NAME         "alignment_kd"
-#define PARAM_ALIGNMENT_KD_DEFAULT      0.3
-#define PARAM_ALIGNMENT_KI_NAME         "alignment_ki"
-#define PARAM_ALIGNMENT_KI_DEFAULT      0.0
-#define PARAM_ALIGNMENT_SPEED_NAME      "alignment_speed"
-#define PARAM_ALIGNMENT_SPEED_DEFAULT   0.5
-
 using namespace s8;
 using namespace s8::go_straight_controller_node;
 using namespace s8::pid;
 using namespace s8::utils::math;
 
-#define IR_INVALID_VALUE ir_sensors_node::TRESHOLD_VALUE
-
 class GoStraight : public Node {
-    ros::Subscriber ir_distances_subscriber;
     ros::Subscriber pose_subscriber;
     ros::Publisher twist_publisher;
     actionlib::SimpleActionServer<s8_go_straight_controller::GoStraightAction> go_straight_action;
     actionlib::SimpleActionClient<s8_motor_controller::StopAction> stop_action;
 
-    double distance;
-    double ir_threshold;
-    PIDController follow_pid;
     PIDController align_pid;
-    PIDController distance_pid;
 
-    double left_front;
-    double left_back;
-    double right_front;
-    double right_back;
-
-    double v;
-    double w;
-    double linear_speed;
-
-    WallToFollow wall_to_follow;
-    bool following;
-    bool preempted;
-
+    bool aligning, recieved_goal;
     int robot_rotation;
+    int goal_roation;
 
-    bool aligning;
-    int alignment_streak;
-    int alignment_angle;
-    double alignment_speed;
+    bool going;
+    double linear_speed;
+    double v, w;
 
 public:
-    GoStraight(int hz) : distance_pid (hz), follow_pid(hz), align_pid(hz), v(0.0), w(0.0), alignment_streak(0), alignment_angle(0), aligning(false), following(false), preempted(false), left_front(IR_INVALID_VALUE), left_back(IR_INVALID_VALUE), right_front(IR_INVALID_VALUE), right_back(IR_INVALID_VALUE), stop_action(ACTION_STOP, true), go_straight_action(nh, ACTION_FOLLOW_WALL, boost::bind(&GoStraight::action_execute_go_straight_callback, this, _1), false) {
-        init_params();
-        print_params();
-
-        ir_distances_subscriber = nh.subscribe<s8_msgs::IRDistances>(TOPIC_IR_DISTANCES, 1, &GoStraight::ir_distances_callback, this);
-                pose_subscriber = nh.subscribe<geometry_msgs::Pose2D>(TOPIC_POSE, 1, &GoStraight::pose_callback, this);
-
+    GoStraight(int hz) : align_pid(hz), v(0.0), w(0.0), aligning(false), going(false), stop_action(ACTION_STOP, true), go_straight_action(nh, ACTION_GO_STRAIGHT, boost::bind(&GoStraight::action_execute_go_straight_callback, this, _1), false) {
+        pose_subscriber = nh.subscribe<geometry_msgs::Pose2D>(TOPIC_POSE, 1, &GoStraight::pose_callback, this);
         twist_publisher = nh.advertise<geometry_msgs::Twist>(TOPIC_TWIST, 1);
         
         go_straight_action.registerPreemptCallback(boost::bind(&GoStraight::go_straight_cancel_callback, this));
-
         go_straight_action.start();
 
         ROS_INFO("Waiting for stop action server...");
@@ -108,56 +53,10 @@ public:
     }
 
     void update() {
-        if(!following) {
+        if(!going) {
             return;
         }
-
-        double back = wall_to_follow == WallToFollow::LEFT ? left_back : right_back;
-        double front = wall_to_follow == WallToFollow::LEFT ? left_front : right_front;
-
-        //TODO: Use sign instead in utils.
-        auto get_alignment_angle = [&back, &front]() {
-            return back - front >= 0 ? 1 : -1;
-        };
-
-        if(is_ir_valid_value(back) && is_ir_valid_value(front)) {
-            if(aligning) {
-                if(alignment_angle != 0) {
-                    if(alignment_angle != get_alignment_angle()) {
-                        ROS_INFO("Alignment angle sign changed.");
-                        align_pid.reset();
-                    }
-                }
-
-                alignment_angle = get_alignment_angle();
-
-                if(is_aligned(back, front)) {
-                    if(alignment_streak == 1) {
-                        ROS_INFO("Seconds alignment check");
-                        aligning = false;
-                        ROS_INFO("Alignment complete!");
-                        v = 0.0;
-                        w = 0.0;
-                    } else {
-                        ROS_INFO("First alignment check");
-                        alignment_streak++;
-                        stop();
-                        w = 0.0;
-                    }
-                } else {
-                    alignment_streak = 0;
-                    align_controller(back, front, wall_to_follow);
-                }
-            }
-
-            if(!aligning) {
-                follow_controller(back, front, wall_to_follow);
-            }
-        } else {
-            ROS_INFO("Stopped following wall due to invalid ir sensor values. back: %lf, front: %lf", back, front);
-            following = false;
-            return;
-        }
+        //Do controller stuff:
 
         publish();
     }
@@ -179,134 +78,20 @@ public:
     }
     
 private:
-    bool is_aligned(double back, double front) {
-        return std::abs(std::abs(back) - std::abs(front)) <= 0.02;
-    }
-
     void go_straight_cancel_callback() {
-        stop_following();
-
+        stop_going_straight();
         ROS_INFO("Cancelling go straight action...");
     }
 
-    void stop_following() {
+    void stop_going_straight() {
         aligning = false;
-        following = false;
-        preempted = true;
+        going = false;
         v = 0.0;
         w = 0.0;
     }
 
     void action_execute_go_straight_callback(const s8_go_straight_controller::FollowWallGoalConstPtr & goal) {
-        preempted = false;
-        following = true;
-        aligning = true;
         
-        wall_to_follow = WallToFollow(goal->wall_to_follow);
-
-        const int timeout = 30; // 30 seconds.
-        const int rate_hz = 10;
-
-        ros::Rate rate(rate_hz);
-
-        int ticks = 0;
-
-        ROS_INFO("Wall following action started. Wall to follow: %s", to_string(wall_to_follow).c_str());
-
-        follow_pid.reset();
-        distance_pid.reset();
-        align_pid.reset();
-        alignment_streak = 0;
-        alignment_angle = 0;
-        while(ros::ok() && following && ticks <= timeout * rate_hz) {
-            rate.sleep();
-            ticks++;
-        }
-
-        if(!ros::ok()) {
-            ROS_INFO("Ros not OK");
-            return;
-        }
-
-        if(ticks >= timeout * rate_hz) {
-            ROS_WARN("Wall following action timed out.");
-            stop_following();
-            s8_go_straight_controller::GoStraightResult go_straight_action_result;
-            go_straight_action_result.reason = GoStraightFinishedReason::TIMEOUT;
-            go_straight_action.setAborted(go_straight_action_result);
-        } else if(preempted) {
-            ROS_INFO("PREEMPTED: Cancelled wall following");
-            s8_go_straight_controller::GoStraightResult go_straight_action_result;
-            go_straight_action_result.reason = GoStraightFinishedReason::PREEMPTED;
-            go_straight_action.setPreempted(go_straight_action_result);        
-        } else if(!preempted) {
-            ROS_INFO("SUCCEEDED: Wall following succeeded");
-            stop_following();
-            s8_go_straight_controller::GoStraightResult go_straight_action_result;
-            go_straight_action_result.reason = GoStraightFinishedReason::OUT_OF_RANGE;
-            go_straight_action.setSucceeded(go_straight_action_result);
-        }
-    }
-
-    double get_controller_diff(double back, double front, int away_direction) {
-        return away_direction * (back - front);
-    }
-
-    void follow_controller(double back, double front, int towards_direction, bool do_distance = true) {
-        int away_direction = -towards_direction;
-
-        double diff = get_controller_diff(back, front, away_direction);
-        double average = (back + front) / 2;
-
-        w = 0.0;
-
-        follow_pid.update(w, diff);
-
-        v = linear_speed;
-
-        // Maybe should change kp_near and kp_far not to be used when aligning?Z
-        if(do_distance) {
-            double distance_diff = distance - average;
-            distance_controller(distance_diff, towards_direction);
-        }
-
-        ROS_INFO("Follow controller back: %.2lf, front: %.2lf, diff %lf, prev_diff: %.2lf, w: %.2lf", back, front, diff, follow_pid.get_prev_error(), w);
-    }
-
-    void distance_controller(double distance_diff, int towards_direction){
-        int away_direction = -towards_direction;
-        double diff = distance_diff * away_direction;
-        // Add back the kp_far and near difference perhaps?
-        if (distance_diff < 0) {
-            int old_kp = distance_pid.kp;
-            distance_pid.kp /= 5;
-            distance_pid.update(w,diff);
-            distance_pid.kp = old_kp;
-        }
-        else {
-            distance_pid.update(w, diff);
-        }
-        ROS_INFO("distance difference: %lf",  diff);
-    }
-
-    void align_controller(double back, double front, int towards_direction) {
-        double away_direction = -towards_direction;
-        double diff = get_controller_diff(back, front, away_direction);
-        w = sign(diff) * alignment_speed;
-        //align_pid.update(w, diff);
-        v = 0.0;
-        ROS_INFO("Alignment controller back: %.2lf, front: %.2lf, diff %lf, prev_diff: %.2lf, w: %.2lf, towards: %d", back, front, diff, align_pid.get_prev_error(), w, towards_direction);
-    }
-
-    void ir_distances_callback(const s8_msgs::IRDistances::ConstPtr & ir_distances) {
-        left_front = ir_distances->left_front;
-        left_back = ir_distances->left_back;
-        right_front = ir_distances->right_front;
-        right_back = ir_distances->right_back;
-    }
-
-    bool is_ir_valid_value(double value) {
-        return ir_sensors_node::is_valid_ir_value(value) && value < ir_threshold;
     }
 
     void publish() {
@@ -314,7 +99,7 @@ private:
         twist.linear.x = v;
         // Check for too high angular speed
         if (w > 1.5 || w < -1.5)  {
-            ROS_WARN("Possibly bad IR sensor value");
+            //ROS_WARN("Possibly bad IR sensor value");
             w = 0;  
         }
 
@@ -323,32 +108,17 @@ private:
     }
 
     void init_params() {
-        add_param(PARAM_FOLLOWING_KP_NAME, follow_pid.kp, PARAM_FOLLOWING_KP_DEFAULT);
-        add_param(PARAM_FOLLOWING_KD_NAME, follow_pid.kd, PARAM_FOLLOWING_KD_DEFAULT);
-        add_param(PARAM_FOLLOWING_KI_NAME, follow_pid.ki, PARAM_FOLLOWING_KI_DEFAULT);
-        add_param(PARAM_DISTANCE_KI_NAME, distance_pid.ki, PARAM_DISTANCE_KI_DEFAULT);
-        add_param(PARAM_DISTANCE_KP_NAME, distance_pid.kp, PARAM_DISTANCE_KP_DEFAULT);
-        add_param(PARAM_DISTANCE_KD_NAME, distance_pid.kd, PARAM_DISTANCE_KD_DEFAULT);
-        add_param(PARAM_I_LIMIT_NAME, distance_pid.i_limit, PARAM_I_LIMIT_DEFAULT);
-        add_param(PARAM_DISTANCE_NAME, distance, PARAM_DISTANCE_DEFAULT);
-        add_param(PARAM_LINEAR_SPEED_NAME, linear_speed, PARAM_LINEAR_SPEED_DEFAULT);
-        add_param(PARAM_IR_THRESHOLD_NAME, ir_threshold, PARAM_IR_THRESHOLD_DEFAULT);
-        add_param(PARAM_ALIGNMENT_KP_NAME, align_pid.kp, PARAM_ALIGNMENT_KP_DEFAULT);
-        add_param(PARAM_ALIGNMENT_KD_NAME, align_pid.kd, PARAM_ALIGNMENT_KD_DEFAULT);
-        add_param(PARAM_ALIGNMENT_KI_NAME, align_pid.ki, PARAM_ALIGNMENT_KI_DEFAULT);
-        add_param(PARAM_ALIGNMENT_SPEED_NAME, alignment_speed, PARAM_ALIGNMENT_SPEED_DEFAULT);
+        
     }
 };
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, NODE_NAME);
-
-    GoStraight wall_follower(HZ);
-
+    GoStraight go_straight(HZ);
     ros::Rate loop_rate(HZ);
 
     while(ros::ok()) {
-        wall_follower.update();
+        go_straight.update();
         ros::spinOnce();
         loop_rate.sleep();
     }
